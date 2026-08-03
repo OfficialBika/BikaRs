09ebbbfd2e2d349d7edeedd8161fc24b7dcb6f0e
Official Bika <phoetaay@gmail.com>
2026-08-03 15:53:33 +0700

    Add files via upload

--- migrations/add_indexes.js
+++ migrations/add_indexes.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const Reaction = require('../models/Reaction');
+const Report = require('../models/Report');
+const PremiumUser = require('../models/PremiumUser');
 
+async function migrateIndexes() {
+  await Promise.all([
+    User.syncIndexes(),
+    Reaction.syncIndexes(),
+    Report.syncIndexes(),
+    PremiumUser.syncIndexes(),
+  ]);
+}
+
+module.exports = { migrateIndexes };
